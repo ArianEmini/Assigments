adbf7a650ab0daff128c284a22e6c44a4c4df688
Aemini491@gmail.com <Aemini491@gmial.com>
2022-02-22 22:29:54 +0100

    fix problem in first assigment

--- First-assigment/first-app/src/App.js
+++ First-assigment/first-app/src/App.js
@@ -26,51 +26,50 @@ const list=[
     paragraf:"How complte is the actual domain name? "
 
   },
-
   {
-  //  image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
-    imageTwo:"https://icon-library.com/images/recognition-icon/recognition-icon-11.jpg",
+    image:"https://icon-library.com/images/recognition-icon/recognition-icon-11.jpg",
+    Title:"The TLD",
+    paragraf:"Does the domain extension math the language of the domain name"
+
+  },
+  {
+    image:"https://cdn4.vectorstock.com/i/1000x1000/16/98/search-tools-icon-vector-6201698.jpg",
+    // image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
+
+    Title:"Domain Length",
+    paragraf:"is the domain short enaugh to rember? "
+
+  },
+  {
+    image:"https://wearedapa.co.uk/wp-content/uploads/2017/06/outreach-icon.jpg",
+    // image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
     // image:{image1},
-    TitleTwo:"International recognition",
-    paragrafTwo:"Can the domain name be used on an international sole  "
+    Title:"Language",
+    paragraf:"How complte is the actual domain name? "
 
   },
   {
-    imageTwo:"https://cdn4.vectorstock.com/i/1000x1000/16/98/search-tools-icon-vector-6201698.jpg",
-    // image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
-    TitleTwo:"Search engine",
-    paragrafTwo:"Does the doain follow search ergine gudilens"
+    image:"https://cdn4.vectorstock.com/i/1000x1000/77/58/costs-optimization-efficiency-icon-vector-23097758.jpg",
+    Title:"The TLD",
+    paragraf:"Does the domain extension math the language of the domain name"
 
   },
   {
-    imageTwo:"https://wearedapa.co.uk/wp-content/uploads/2017/06/outreach-icon.jpg",
+    image:"https://www.shareicon.net/data/512x512/2016/10/20/846415_blue_512x512.png",
     // image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
-    TitleTwo:"Advertsing Potential",
-    paragrafTwo:"Could the domain be used for advertsing campsignes "
+
+    Title:"Domain Length",
+    paragraf:"is the domain short enaugh to rember? "
 
   },
   {
-    imageThree:"https://cdn4.vectorstock.com/i/1000x1000/77/58/costs-optimization-efficiency-icon-vector-23097758.jpg",
+    image:"https://img.favpng.com/2/14/25/skill-icon-boosting-potential-icon-business-motivation-icon-HydUTqU5.jpg",
     // image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
-    TitleThree:"Sales",
-    paragrafThree:"Can the domain name be used on an inertational scale "
+    // image:{image1},
+    Title:"Language",
+    paragraf:"How complte is the actual domain name? "
 
-  },
-  {
-    imageThree:"https://www.shareicon.net/data/512x512/2016/10/20/846415_blue_512x512.png",
-    // image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
-    TitleThree:"Typo susceptiility",
-    paragrafThree:"How hight is the risk of mistyping the domain name  "
-
-  },
-  {
-    // image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
-    imageThree:"https://img.favpng.com/2/14/25/skill-icon-boosting-potential-icon-business-motivation-icon-HydUTqU5.jpg",
-    TitleThree:"Business Potential",
-    paragrafThree:"Can the domin be used as your company "
-
-  },
- 
+  }
 ];
 
 const cards=[
@@ -151,24 +150,8 @@ function App() {
                   
     
             </div>
-            <div>
-                  <img src={lis.imageTwo}/>
-                  
-                  
-                  <strong>{lis.TitleTwo}</strong>
-                  <p>{lis.paragrafTwo} </p>
-                  
-    
-            </div>
-            <div>
-                  <img src={lis.imageThree}/>
-                  
-                  
-                  <strong>{lis.TitleThree}</strong>
-                  <p>{lis.paragrafThree} </p>
-                  
-    
-            </div>
+            
+           
     
     
         </div>
