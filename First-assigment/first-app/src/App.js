06f4e3b0b4567568196877a4eb49b54451adc4b8
Aemini491@gmail.com <Aemini491@gmial.com>
2022-02-22 17:14:52 +0100

    change first-app

--- First-assigment/first-app/src/App.js
+++ First-assigment/first-app/src/App.js
@@ -29,45 +29,45 @@ const list=[
 
   {
   //  image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
-    image:"https://icon-library.com/images/recognition-icon/recognition-icon-11.jpg",
+    imageTwo:"https://icon-library.com/images/recognition-icon/recognition-icon-11.jpg",
     // image:{image1},
-    Title:"International recognition",
-    paragraf:"Can the domain name be used on an international sole  "
+    TitleTwo:"International recognition",
+    paragrafTwo:"Can the domain name be used on an international sole  "
 
   },
   {
-    image:"https://cdn4.vectorstock.com/i/1000x1000/16/98/search-tools-icon-vector-6201698.jpg",
+    imageTwo:"https://cdn4.vectorstock.com/i/1000x1000/16/98/search-tools-icon-vector-6201698.jpg",
     // image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
-    Title:"Search engine",
-    paragraf:"Does the doain follow search ergine gudilens"
+    TitleTwo:"Search engine",
+    paragrafTwo:"Does the doain follow search ergine gudilens"
 
   },
   {
-    image:"https://wearedapa.co.uk/wp-content/uploads/2017/06/outreach-icon.jpg",
+    imageTwo:"https://wearedapa.co.uk/wp-content/uploads/2017/06/outreach-icon.jpg",
     // image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
-    Title:"Advertsing Potential",
-    paragraf:"Could the domain be used for advertsing campsignes "
+    TitleTwo:"Advertsing Potential",
+    paragrafTwo:"Could the domain be used for advertsing campsignes "
 
   },
   {
-    image:"https://cdn4.vectorstock.com/i/1000x1000/77/58/costs-optimization-efficiency-icon-vector-23097758.jpg",
+    imageThree:"https://cdn4.vectorstock.com/i/1000x1000/77/58/costs-optimization-efficiency-icon-vector-23097758.jpg",
     // image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
-    Title:"Sales",
-    paragraf:"Can the domain name be used on an inertational scale "
+    TitleThree:"Sales",
+    paragrafThree:"Can the domain name be used on an inertational scale "
 
   },
   {
-    image:"https://www.shareicon.net/data/512x512/2016/10/20/846415_blue_512x512.png",
+    imageThree:"https://www.shareicon.net/data/512x512/2016/10/20/846415_blue_512x512.png",
     // image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
-    Title:"Typo susceptiility",
-    paragraf:"How hight is the risk of mistyping the domain name  "
+    TitleThree:"Typo susceptiility",
+    paragrafThree:"How hight is the risk of mistyping the domain name  "
 
   },
   {
     // image:"https://www.verisign.com/en_US/resources/img/VRSN_SocialShare-com-Logo_201712.png",
-    image:"https://img.favpng.com/2/14/25/skill-icon-boosting-potential-icon-business-motivation-icon-HydUTqU5.jpg",
-    Title:"Business Potential",
-    paragraf:"Can the domin be used as your company "
+    imageThree:"https://img.favpng.com/2/14/25/skill-icon-boosting-potential-icon-business-motivation-icon-HydUTqU5.jpg",
+    TitleThree:"Business Potential",
+    paragrafThree:"Can the domin be used as your company "
 
   },
  
@@ -145,10 +145,27 @@ function App() {
          
             <div>
                   <img src={lis.image}/>
-                  
-                  
+
                   <strong>{lis.Title}</strong>
                   <p>{lis.paragraf} </p>
+                  
+    
+            </div>
+            <div>
+                  <img src={lis.imageTwo}/>
+                  
+                  
+                  <strong>{lis.TitleTwo}</strong>
+                  <p>{lis.paragrafTwo} </p>
+                  
+    
+            </div>
+            <div>
+                  <img src={lis.imageThree}/>
+                  
+                  
+                  <strong>{lis.TitleThree}</strong>
+                  <p>{lis.paragrafThree} </p>
                   
     
             </div>
@@ -173,11 +190,7 @@ function App() {
                  
                   <Card  key={card.date}  Datee={card.date} Color={card.color} Photo={card.photo}  Name={card.name}  Description={card.description}  Button={card.button} ColorName={card.nameColor} />
                   
-                  {/* <Card  key={card.date}  Datee={card.date} Color={card.color} Photo={card.photo}  Name={card.name}  Description={card.description}  Button={card.button} ColorName={card.nameColor}/>
- 
-                  <Card  key={card.date}  Datee={card.date} Color={card.color} Photo={card.photo}  Name={card.name}  Description={card.description}  Button={card.button} ColorName={card.nameColor}/>
-
-     */}
+               
             </div>
 
 
