642ed7f40b7b945889e9f792a62ad1332ef1b301
Aemini491@gmail.com <Aemini491@gmial.com>
2022-02-24 13:08:29 +0100

    card with no react

--- Seconde-assigment/seconde/src/Components/NavbarPages/Domains.js
+++ Seconde-assigment/seconde/src/Components/NavbarPages/Domains.js
@@ -4,6 +4,57 @@ const Domains = (props) => {
   return ( 
 <div className="Body" >
 
+<div className="Card">
+  <div className="Card-content">
+    <h2>.COM</h2>
+    <h5>$5.99/yr</h5>
+    <p>instead of $10.99/yr</p>
+    <button>Buy Now</button>
+ </div>
+ <div className="Card-content">
+    <h2>.COM</h2>
+    <h5>$5.99/yr</h5>
+    <p>instead of $10.99/yr</p>
+    <button>Buy Now</button>
+ </div>
+ <div className="Card-content">
+    <h2>.COM</h2>
+    <h5>$5.99/yr</h5>
+    <p>instead of $10.99/yr</p>
+    <button>Buy Now</button>
+ </div>
+ <div className="Card-content">
+    <h2>.COM</h2>
+    <h5>$5.99/yr</h5>
+    <p>instead of $10.99/yr</p>
+    <button>Buy Now</button>
+ </div>
+ <div className="Card-content">
+    <h2>.COM</h2>
+    <h5>$5.99/yr</h5>
+    <p>instead of $10.99/yr</p>
+    <button>Buy Now</button>
+ </div>
+ <div className="Card-content">
+    <h2>.COM</h2>
+    <h5>$5.99/yr</h5>
+    <p>instead of $10.99/yr</p>
+    <button>Buy Now</button>
+ </div>
+ <div className="Card-content">
+    <h2>.COM</h2>
+    <h5>$5.99/yr</h5>
+    <p>instead of $10.99/yr</p>
+    <button>Buy Now</button>
+ </div>
+ <div className="Card-content">
+    <h2>.COM</h2>
+    <h5>$5.99/yr</h5>
+    <p>instead of $10.99/yr</p>
+    <button>Buy Now</button>
+ </div>
+</div>
+
 
 
 </div>
