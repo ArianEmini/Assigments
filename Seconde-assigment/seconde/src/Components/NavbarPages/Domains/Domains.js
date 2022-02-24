9f3ecf5b5b8101b11ec54e9ea73a9f31412b8b60
Aemini491@gmail.com <Aemini491@gmial.com>
2022-02-25 00:34:08 +0100

    fix icone

--- Seconde-assigment/seconde/src/Components/NavbarPages/Domains/Domains.js
+++ Seconde-assigment/seconde/src/Components/NavbarPages/Domains/Domains.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React  from "react";
+import { Component } from "react";
+import { useState } from "react";
 import "./Domains.css"
 import Paragraf from "./Paragraf";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faAngleDown} from "@fortawesome/free-solid-svg-icons"
-const Domains = (props) => {
+import {faAngleUp} from "@fortawesome/free-solid-svg-icons"
+
+const Domains  = (props) => {
   const DomainsData=[
     {
       name:".COM",
@@ -52,11 +56,31 @@ const Domains = (props) => {
       oldbuy:"$10.99/yr",
       button:"Buy now"
     }
-  ]
+  ];
 
 // useState
 
-  const [OpenmyDomains,setOpenmyDomains]=useState(false)
+  const [OpenmyDomains,setOpenmyDomains]=useState(false);
+  const [state,setState]=useState(false);
+
+  
+// constructor(props){
+//     super(props);
+//     this.state={
+//       isToggleOn:false    
+//     }
+//     this.handleClick=this.handleClick.bind(this);
+//   }
+
+//   function handleClick(){
+//     this.setState(state=>({
+//       isToggleOn:!state.isToggleOn
+//     }))
+//   }
+
+const toggle=()=>{
+  setState(!state)
+}
 
  
 
@@ -83,7 +107,7 @@ const Domains = (props) => {
         <div className="divi">
         <p className="paragraf-domain">Why park a domain name in Parkname?</p>
         <div className="space"></div>
-        <FontAwesomeIcon onClick={()=>setOpenmyDomains(!OpenmyDomains)}   className="icone" icon={faAngleDown}></FontAwesomeIcon>
+        <button onClick={toggle}  className="buttonToggle">{state?<FontAwesomeIcon onClick={()=>setOpenmyDomains(!OpenmyDomains)}   className="icone" icon={faAngleUp}/>:<FontAwesomeIcon onClick={()=>setOpenmyDomains(!OpenmyDomains)}   className="icone" icon={faAngleDown}/>}</button>
         </div>
      {OpenmyDomains && <Paragraf/>  } 
      </div>
